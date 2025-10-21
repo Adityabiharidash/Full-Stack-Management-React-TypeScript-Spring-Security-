5511a5d0a45ec59d4c3f9a70f6ea2d0fa49c07b9
aditya bihari dash <adash2022@gift.edu.in>
2025-10-21 15:39:32 +0530

    All UI Update

--- Full Stack Management (React, TypeScript + Spring, Security)/Backend/Shiwansh_Solutions_Intership_Project/src/main/java/com/nt/repository/IDistrictRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.nt.entity.District;
-
-public interface IDistrictRepository extends JpaRepository<District, Long> {
-
-}
--- Full Stack Management (React, TypeScript + Spring, Security)/Backend/Shiwansh_Solutions_Intership_Project/src/main/java/com/nt/repository/IRepositoryLanguage.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nt.entity.Language;
-
-public interface IRepositoryLanguage extends JpaRepository<Language, Long> {
-
-}
